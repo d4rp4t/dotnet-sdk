3bd065ed6c2a68caf644d643e8dd99f9cf822d1c
jackstar12 <62219658+jackstar12@users.noreply.github.com>
2026-06-23 21:51:36 +0200

    fix: commit hash version field (#143)
    This doesn't exist in the API

--- NArk.Swaps/Boltz/Models/Info/VersionResponse.cs
+++ NArk.Swaps/Boltz/Models/Info/VersionResponse.cs
@@ -6,7 +6,4 @@ public class VersionResponse
 {
     [JsonPropertyName("version")]
     public required string Version { get; set; }
-
-    [JsonPropertyName("commitHash")]
-    public required string CommitHash { get; set; }
 }
